f5373f6eb8c5426fe3f40b6c35ff7e2dcad07138
Wabri <gabriele.puliti@gmail.com>
2018-01-05 17:18:43 +0100

    create points in UserGame class

--- SomeTelegramBot/src/main/java/some/telegram/bot/core/UserGame.java
+++ SomeTelegramBot/src/main/java/some/telegram/bot/core/UserGame.java
@@ -5,13 +5,19 @@ import org.telegram.telegrambots.api.objects.Contact;
 public class UserGame {
 
 	private Contact contact;
+	private int points;
 
 	public UserGame(Contact contact) {
 		this.contact = contact;
+		points = 0;
 	}
 
 	public Contact getContact() {
 		return contact;
+	}
+
+	public int getPoints() {
+		return points;
 	}
 
 }
