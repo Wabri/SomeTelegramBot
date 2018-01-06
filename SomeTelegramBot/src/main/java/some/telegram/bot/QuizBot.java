189cc580422d7764203abc1e2981734740392414
Wabri <gabriele.puliti@gmail.com>
2018-01-06 12:29:23 +0100

    request change in other classes

--- SomeTelegramBot/src/main/java/some/telegram/bot/QuizBot.java
+++ SomeTelegramBot/src/main/java/some/telegram/bot/QuizBot.java
@@ -1,17 +1,43 @@
 package some.telegram.bot;
 
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Contact;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import some.telegram.bot.core.UserGame;
+import some.telegram.bot.manager.ManagerUsersGame;
 
 public class QuizBot extends TelegramLongPollingBot {
 
-	public QuizBot() {
+	ManagerUsersGame managerUsersGame;
 
+	public QuizBot() {
+		managerUsersGame = new ManagerUsersGame();
 	}
 
 	@Override
 	public void onUpdateReceived(Update update) {
+		try {
+			Contact contact = update.getMessage().getContact();
+			if (managerUsersGame.containUserGameContact(contact)) {
+				UserGame userGame = managerUsersGame.getUserGame(contact);
+				SendTextMessage(userGame.getChat().getId(), "Vuoi giocare?");
+			} else {
 
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void SendTextMessage(Long chatId, String text) {
+		try {
+			execute(new SendMessage().setChatId(chatId).setText(text));
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
