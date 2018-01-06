93337b537814c3c100351bcd03abbd3c82fb103d
Wabri <gabriele.puliti@gmail.com>
2018-01-06 12:44:44 +0100

    create keyboardmarkup to answer /start command

--- SomeTelegramBot/src/main/java/some/telegram/bot/QuizBot.java
+++ SomeTelegramBot/src/main/java/some/telegram/bot/QuizBot.java
@@ -1,8 +1,13 @@
 package some.telegram.bot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Contact;
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
@@ -12,22 +17,43 @@ import some.telegram.bot.manager.ManagerUsersGame;
 public class QuizBot extends TelegramLongPollingBot {
 
 	ManagerUsersGame managerUsersGame;
+	List<UserGame> userGameUnknown;
 
 	public QuizBot() {
 		managerUsersGame = new ManagerUsersGame();
+		userGameUnknown = new ArrayList<UserGame>();
 	}
 
 	@Override
 	public void onUpdateReceived(Update update) {
 		try {
 			Contact contact = update.getMessage().getContact();
-			if (managerUsersGame.containUserGameContact(contact)) {
-				UserGame userGame = managerUsersGame.getUserGame(contact);
-				SendTextMessage(userGame.getChat().getId(), "Vuoi giocare?");
+			if (!managerUsersGame.containUserGameContact(contact)) {
+				if (update.getMessage().getText().equals("/start")) {
+					// UserGame userGame = managerUsersGame.getUserGame(contact);
+					UserGame newGamer = new UserGame(contact, update.getMessage().getChat());
+					userGameUnknown.add(newGamer);
+					ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+					List<KeyboardRow> keyboard = new ArrayList<>();
+					KeyboardRow row = new KeyboardRow();
+					row.add("Si!");
+					row.add("No!");
+					keyboard.add(row);
+					keyboardMarkup.setKeyboard(keyboard);
+					SendTextMessageWithKeyboard(newGamer.getChat().getId(), "Vuoi giocare?", keyboardMarkup);
+				}
 			} else {
 
 			}
 		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void SendTextMessageWithKeyboard(Long chatId, String text, ReplyKeyboardMarkup keyboardMarkup) {
+		try {
+			execute(new SendMessage().setChatId(chatId).setText(text).setReplyMarkup(keyboardMarkup));
+		} catch (TelegramApiException e) {
 			e.printStackTrace();
 		}
 	}
